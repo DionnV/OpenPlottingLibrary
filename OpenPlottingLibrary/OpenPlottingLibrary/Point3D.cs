dfcabc8fce5f15821cd872e054573ae977dbb706
Dion van 't Veer <dionnv@gmail.com>
2014-04-18 09:12:09 +0200

    Added some pointgen and point class

---
+++ OpenPlottingLibrary/OpenPlottingLibrary/Point3D.cs
@@ -0,0 +1,16 @@
+﻿namespace OpenPlottingLibrary
+{
+    struct Point3D
+    {
+        public float x { get; set; }
+        public float y { get; set; }
+        public float z { get; set; }
+
+        public Point3D(float x, float y, float z) : this()
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+    }
+}
--- OpenPlottingLibrary/OpenPlottingLibrary/PointGenerator.cs
+++ OpenPlottingLibrary/OpenPlottingLibrary/PointGenerator.cs
@@ -8,6 +8,27 @@ namespace OpenPlottingLibrary
 {
     class PointGenerator
     {
+        public List<Point3D> generatedPoints;
+
+        public PointGenerator() 
+        {
+            generatedPoints = new List<Point3D>();
+        }
+
+
+        public static List<Point3D> Generate(int xMin, int xMax, int yMin, int yMax)
+        {
+            List<Point3D> generatedPoints = new List<Point3D>();
+            for(int x = xMin; x < xMax; x++)
+            {
+                for (int y = yMin; y < yMax; y++)
+                {
+                    generatedPoints.Add(new Point3D(x, y, x + y));
+                }
+            }
+
+            return generatedPoints;
+        }
 
     }
 }
