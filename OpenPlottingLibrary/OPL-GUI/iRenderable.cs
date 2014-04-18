d96318b18e482700f383905d4844af125e445a49
Kenno1991 <kenno1991@gmail.com>
2014-04-18 10:47:50 +0200

    íRenderable
    Renderable interface with draw function

---
+++ OpenPlottingLibrary/OPL-GUI/iRenderable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenPlottingLibrary;
+
+namespace OPL_GUI
+{
+    interface IRenderable
+    {
+        public void draw()
+        {
+            //draw functie openGL
+        }
+    }
+}
