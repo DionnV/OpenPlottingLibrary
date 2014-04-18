afbf74fbaa42c51b30255fa9c77db76157bd0a5e
Dion van 't Veer <dionnv@gmail.com>
2014-04-18 09:16:20 +0200

    Made point3D struct var sets private

--- OpenPlottingLibrary/OpenPlottingLibrary/Point3D.cs
+++ OpenPlottingLibrary/OpenPlottingLibrary/Point3D.cs
@@ -2,9 +2,9 @@
 {
     struct Point3D
     {
-        public float x { get; set; }
-        public float y { get; set; }
-        public float z { get; set; }
+        public float x { get; private set; }
+        public float y { get; private set; }
+        public float z { get; private set; }
 
         public Point3D(float x, float y, float z) : this()
         {
