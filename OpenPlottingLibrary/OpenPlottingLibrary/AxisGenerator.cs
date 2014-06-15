3ab7a60d9fedfc39a8e9ed10b75aa215fa6a6376
Kenney <kenno1991@gmail.com>
2014-06-15 15:44:09 +0200

    Added AxisGenerator

---
+++ OpenPlottingLibrary/OpenPlottingLibrary/AxisGenerator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenPlottingLibrary
+{
+    public class AxisGenerator
+    {
+        public List<Point3D> generatedAxis;
+        public enum axis {x,y,z}
+
+        public AxisGenerator() 
+        {
+            generatedAxis = new List<Point3D>();
+        }
+
+        public static List<Point3D> Generate(axis ax ,int Min, int Max)
+        {
+            List<Point3D> generatedAxis = new List<Point3D>();
+
+            switch(ax)
+            {
+                case axis.x:
+                    for (float i = Min; i <= Max; i = (float)(i + .1))
+                    {
+                        generatedAxis.Add(new Point3D(i, 0, 0));
+                    }
+                    break;
+                case axis.y:
+                    for (float i = Min; i <= Max; i = (float)(i + .1))
+                    {
+                        generatedAxis.Add(new Point3D(0, i, 0));
+                    }
+                    break;
+                case axis.z:
+                    for (float i = Min; i <= Max; i = (float)(i + .1))
+                    {
+                        generatedAxis.Add(new Point3D(0, 0, i));
+                    }
+                    break;
+            }
+
+            return generatedAxis;
+        }
+    }
+}
